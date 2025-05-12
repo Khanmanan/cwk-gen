f132fd7282cae569f8182a4f0de4c4ff67376d14
Cwkhan <65243374+Khanmanan@users.noreply.github.com>
2025-05-12 21:37:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ const { generateRankCard } = require('./generators/rank');
 const { generateProfileCard } = require('./generators/profile');
 const { generateServerBanner } = require('./generators/banner');
 const { loadFont, registerFonts } = require('./utils');
-const { generateAnimatedWelcome } = require('./generators/animatedwelcome');
 
 module.exports = {
     generateWelcomeImage,
-    generateAnimatedWelcome, 
     generateRankCard,
     generateProfileCard,
     generateServerBanner,
